ff5625331bcb144e18978f9c61416e52c27b1229
nthomas211 <nthomas21@hawk.iit.edu>
2025-03-06 14:53:51 -0600

    Reordered CSS and added js

--- js/site.js
+++ js/site.js
@@ -1,5 +1,10 @@
-// jQuery 3.x-style ready event and locally scoped $
-jQuery(function($) {
-  $('html').removeClass('nojs');
-  $('html').addClass('hasjs');
+document.addEventListener("DOMContentLoaded", function () {
+    let formContainer = document.querySelector(".form-container");
+    formContainer.style.opacity = 0; // Start with hidden form
+    formContainer.style.transition = "opacity 1.5s ease-in-out"; // Smooth transition
+
+    setTimeout(() => {
+        formContainer.style.opacity = 1; // Fade in
+    }, 500); // Delay before fade-in starts
 });
+
