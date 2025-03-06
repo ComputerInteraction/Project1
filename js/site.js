643eb7ab624e8b028a053fd238e4184c2ac6cf8c
nthomas211 <nthomas21@hawk.iit.edu>
2025-03-06 16:28:45 -0600

    Fixed overflow issue with inputs and removed java code

--- js/site.js
+++ js/site.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let formContainer = document.querySelector(".form-container");
-    formContainer.style.opacity = 0; // Start with hidden form
-    formContainer.style.transition = "opacity 1.5s ease-in-out"; // Smooth transition
-
-    setTimeout(() => {
-        formContainer.style.opacity = 1; // Fade in
-    }, 500); // Delay before fade-in starts
-});
 
